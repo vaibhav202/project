acbe91a19ed61dc2f21be99f36a96f585a043b43
vaibhavpancholi05 <71831734+vaibhavpancholi05@users.noreply.github.com>
2021-12-07 20:19:43 +0530

    project: homepage of the site

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=windows-1252">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;400;500;700;800;900&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <link href="style1.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="nav">
+        <label class="brand">ShareTheRide!</label>
+        <div class="navlinks">
+            <a href="#Login/Signup">Login/Signup</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
